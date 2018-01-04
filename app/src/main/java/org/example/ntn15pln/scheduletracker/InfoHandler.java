2728537fa528b343e8bf7a040227076250ab3ef3
Philip Larsson <lormiz@gmail.com>
2018-01-04 15:06:06 +0100

    Added separation between date and time.

--- app/src/main/java/org/example/ntn15pln/scheduletracker/InfoHandler.java
+++ app/src/main/java/org/example/ntn15pln/scheduletracker/InfoHandler.java
@@ -1,18 +1,40 @@
 package org.example.ntn15pln.scheduletracker;
 
 public class InfoHandler {
-    private String courseName, courseCode, programCode, lectureInfo, start, stop, roomNr, teacherSignature, secondTeacherSignature;
+    private String courseName, courseCode, programCode, lectureInfo, start, stop, startDate, stopDate, startTime, stopTime, roomNr, teacherSignature, secondTeacherSignature;
 
     public void setCourseName(String courseName) { this.courseName = courseName; }
     public void setCourseCode(String courseCode) { this.courseCode = courseCode; }
     public void setProgramCode(String programCode) { this.programCode = programCode; }
     public void setLectureInfo(String lectureInfo) { this.lectureInfo = lectureInfo; }
-    public void setStart(String start) { this.start = start; }
-    public void setStop(String stop) { this.stop = stop; }
+
+    public void setStart(String start) {
+        this.start = start;
+        setStartDate();
+        setStartTime();
+    }
+
+    public void setStop(String stop) {
+        this.stop = stop;
+        setStopDate();
+        setStopTime();
+    }
+
     public void setRoomNr(String roomNr) { this.roomNr = roomNr; }
     public void setSecondTeacherSignature(String secondTeacherSignature) { this.secondTeacherSignature = secondTeacherSignature; }
     public void setTeacherSignature(String teacherSignature) { this.teacherSignature = teacherSignature; }
 
+
+    public void setStartDate() { startDate = start.substring(0, start.lastIndexOf("T")); }
+    public void setStartTime() { startTime = start.substring(start.lastIndexOf("T")); }
+    public void setStopDate() { stopDate = stop.substring(0, stop.lastIndexOf("T")); }
+    public void setStopTime() { stopTime = stop.substring(stop.lastIndexOf("T"));}
+
+
+    public String getStartDate() { return startDate; }
+    public String getStartTime() { return startTime; }
+    public String getStopDate() { return stopDate; }
+    public String getStopTime() { return stopTime; }
     public String getCourseName() { return courseName; }
     public String getCourseCode() { return courseCode; }
     public String getProgramCode() { return programCode; }
